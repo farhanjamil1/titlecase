01e4a8d41acc341f0b9b58316b84058fd7986dcf
farhanjamil1 <150520713+farhanjamil1@users.noreply.github.com>
2023-11-26 04:54:48 -0800

    title case

---
+++ titlecase.ts
@@ -0,0 +1,21 @@
+let personName = "farhan jamil";
+let words = personName.split(" ");
+
+let titleCaseName = ""
+
+// console.log (words[0].charAt(3).toUpperCase());
+
+// console.log(words[1].split(""))
+
+// console.log(words[0].slice(2))
+
+// console.log(words[0].slice(1).toUpperCase())
+
+for (let i = 0 ; i < words.length; i++) {
+titleCaseName +=words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase()+ " "
+
+};
+
+// titleCaseName +=words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase(); 
+console.log (titleCaseName);
+
